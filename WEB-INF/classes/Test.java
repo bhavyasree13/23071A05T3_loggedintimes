4d2f845e62f6bd9862ac99c5f2a655e1a28adad3
bhavyasree13 <bhavyasreenuthalapati@gmail.com>
2025-05-20 14:54:06 +0530

    first commit

--- WEB-INF/classes/Test.java
+++ WEB-INF/classes/Test.java
@@ -3,35 +3,37 @@ import java.io.PrintWriter;
 import javax.servlet.ServletException; 
 import javax.servlet.annotation.WebServlet; 
 import javax.servlet.http.*;
-import java.util.*;
+
+@WebServlet("/Test") // optional annotation to map servlet
 public class Test extends HttpServlet { 
-    protected void doPost(HttpServletRequest request, 
-HttpServletResponse response) 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException { 
-        String firstname = request.getParameter("firstname"); 
-        String lastname = request.getParameter("lastname"); 
-        String dob = request.getParameter("dob");  
-        String age = request.getParameter("age"); 
-        String city = request.getParameter("city"); 
-        String email = request.getParameter("email"); 
+        HttpSession session = request.getSession(); 
         String username = request.getParameter("username"); 
-        String password = request.getParameter("password"); 
-        String key = "";
-        // Generate key: first two letters of firstname + dob 
-        if (firstname.length() >= 2) { 
-            key = firstname.substring(0, 2) + dob; 
+        Integer count = (Integer) session.getAttribute("count"); 
+
+        if (count == null) { 
+            count = 1; 
         } else { 
-            key = firstname+ dob; 
+            count++; 
         } 
+
+        session.setAttribute("count", count); 
+        session.setAttribute("username", username); 
+
         response.setContentType("text/html"); 
         PrintWriter out = response.getWriter(); 
-        out.println("<h2>Registration Successful!</h2>"); 
-        out.println("<p>Name: " + firstname + " " + lastname + "</p>"); 
-        out.println("<p>Username: " + username + "</p>"); 
-        out.println("<p>Key: " + key + "</p>"); 
-        out.println("<p>DOB: " + dob + "</p>"); 
-        out.println("<p>Age: " + age + "</p>"); 
-        out.println("<p>City: " + city + "</p>"); 
-        out.println("<p>Email: " + email + "</p>"); 
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head><title>Login Info</title></head>");
+        out.println("<body style='font-family: Arial, sans-serif; background-color: #f2f2f2; text-align: center; padding-top: 50px;'>");
+
+        out.println("<div style='background-color: white; border-radius: 10px; width: 400px; margin: auto; padding: 20px; box-shadow: 0px 0px 10px #ccc;'>");
+        out.println("<h2 style='color: #333;'>Hello, <span style='color: #0066cc;'>" + username + "</span>!</h2>");
+        out.println("<p style='font-size: 18px;'>You have logged in <strong>" + count + "</strong> times.</p>");
+        out.println("</div>");
+
+        out.println("</body></html>");
     } 
-} 
+}
